5bab1e61660cc3e0480e5c06c6e299a8009fe0f0
Alberto Miranda <Alberto.Miranda@opendeusto.es>
2017-10-31 12:27:05 +0100

    Paquetes añadidos: Ventanas,Utilidades y Prueba. || Clases añadidas: Loginregister(falta comprobacion con bd).

---
+++ ReservasDeAerolinea/src/utilidades/LoginRegister.java
@@ -0,0 +1,17 @@
+package utilidades;
+
+public class LoginRegister {
+	public static boolean log(String user, String pass) {
+		boolean logSuccess = false;
+		//Comprobar campos que esten correctos
+		//Comprobacion en BD si existe user con ese nom y pass
+		return logSuccess;		
+	}
+	public static boolean reg(String user, String pass, String confirPass) {
+		//falta añadir mas atributos que recibira este metodo para registrar un user
+		boolean regSuccess = false;
+		//Comprobar campos que esten correctos
+		//añadir a la BD dichos datos
+		return regSuccess;
+	}
+}
