a1a23f9a6dcf4b75d35ce48d83432bc2c1ffc1aa
Iñigo <Iigo@192.168.1.43>
2017-11-08 18:05:41 +0100

    Estructura básica reserva

--- ReservasDeAerolinea/src/ventanas/VInicio.java
+++ ReservasDeAerolinea/src/ventanas/VInicio.java
@@ -13,12 +13,17 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class VInicio extends JFrame {
 	
 	private Usuario loggedUser;
-
-	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 	
 	public static void crearVInicio(Usuario u) {
 		EventQueue.invokeLater(new Runnable() {
@@ -44,15 +49,85 @@ public class VInicio extends JFrame {
 		}
 		this.loggedUser = u;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setBounds(100, 100, 474, 490);
+		getContentPane().setLayout(null);
 		
-		JLabel lblWelcome = new JLabel("Bienvenido, " + u.getName());
-		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(lblWelcome, BorderLayout.CENTER);
+		JLabel lblReservaDeVuelos = new JLabel("RESERVA DE VUELOS");
+		lblReservaDeVuelos.setBounds(12, 13, 154, 16);
+		getContentPane().add(lblReservaDeVuelos);
+		
+		JRadioButton rdbtnIdaYVuelta = new JRadioButton("Ida y Vuelta");
+		rdbtnIdaYVuelta.setBounds(8, 38, 127, 25);
+		getContentPane().add(rdbtnIdaYVuelta);
+		
+		JRadioButton rdbtnSoloIda = new JRadioButton("Solo Ida");
+		rdbtnSoloIda.setBounds(156, 38, 127, 25);
+		getContentPane().add(rdbtnSoloIda);
+		
+		JLabel lblOrigen = new JLabel("ORIGEN");
+		lblOrigen.setBounds(12, 83, 56, 16);
+		getContentPane().add(lblOrigen);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"BILBAO", "MADRID", "BARCELONA", "PARIS", "BERLIN", "ROMA", "LISBOA"}));
+		comboBox.setBounds(80, 80, 132, 22);
+		getContentPane().add(comboBox);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"BILBAO", "MADRID", "BARCELONA", "PARIS", "BERLIN", "ROMA", "LISBOA"}));
+		comboBox_1.setBounds(80, 115, 132, 22);
+		getContentPane().add(comboBox_1);
+		
+		JLabel lblDestino = new JLabel("DESTINO");
+		lblDestino.setBounds(12, 118, 56, 16);
+		getContentPane().add(lblDestino);
+		
+		JLabel lblNPasajeros = new JLabel("N\u00BA PASAJEROS");
+		lblNPasajeros.setBounds(12, 159, 98, 16);
+		getContentPane().add(lblNPasajeros);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"}));
+		comboBox_2.setBounds(122, 156, 41, 22);
+		getContentPane().add(comboBox_2);
+		
+		JLabel lblTipoBillete = new JLabel("CLASE");
+		lblTipoBillete.setBounds(198, 159, 87, 16);
+		getContentPane().add(lblTipoBillete);
+		
+		JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"Turista", "Business"}));
+		comboBox_3.setBounds(312, 156, 87, 22);
+		getContentPane().add(comboBox_3);
+		
+		JLabel lblFechaIda = new JLabel("FECHA IDA");
+		lblFechaIda.setBounds(12, 188, 80, 16);
+		getContentPane().add(lblFechaIda);
+		
+		JLabel lblFechaVuelta = new JLabel("FECHA VUELTA");
+		lblFechaVuelta.setBounds(12, 224, 98, 16);
+		getContentPane().add(lblFechaVuelta);
+		
+		textField = new JTextField();
+		textField.setBounds(122, 191, 126, 22);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(122, 221, 126, 22);
+		getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblEjemploDdmmaaaa = new JLabel("Ejemplo: DD/MM/AAAA");
+		lblEjemploDdmmaaaa.setBounds(260, 194, 154, 16);
+		getContentPane().add(lblEjemploDdmmaaaa);
+		
+		JButton btnAceptar = new JButton("ACEPTAR");
+		btnAceptar.setBounds(122, 405, 97, 25);
+		getContentPane().add(btnAceptar);
+		
+		JButton btnCancelat = new JButton("CANCELAR");
+		btnCancelat.setBounds(260, 405, 97, 25);
+		getContentPane().add(btnCancelat);
 	}
-
 }
